b03a02e0dd10423d4daec552815d66b5333ed631
aotoole <aotoole@fwparker.org>
2017-04-24 09:45:31 -0400

    Final

--- tkinter_8ball.py
+++ tkinter_8ball.py
@@ -12,3 +12,62 @@
 # - Add a Label widget to display the answer (set to a initial value of "Your Fortune Here" or "--" or similar)
 # - Get your random answer message from a list of at least 10 possible strings. (e.g. ["Yes", "No", "Most Likely", "Definitely", etc...])
 # - Add THREE or more other style modifications to make your app unique (font family, font size, color, padding, image, borders, justification, whatever you can find in tkinter library etc.)  Make a comment at the top or bottom of your code to tell me your 3 things you did. (Just to help me out in checking your assignment)
+
+#fbot.com to look up
+#tkinter 8.5 reference
+
+##############Modified the format of the title, fonts, and color
+
+
+from tkinter import *
+from tkinter.font import *
+import random
+
+class App():
+    def __init__(self, master):
+
+        #Title Font
+        self.title_font = Font(family='Comic Sans MS', size=30)
+        self.other_font = Font(family = 'Curlz MT', size = 20)
+
+        #Variables
+        self.entry_question = DoubleVar()
+        self.entry_question.set('')
+
+        self.fortune_text = DoubleVar()
+        self.fortune_text.set("Get Your Fortune Here!")
+
+        self.fortunes = ['Yes.', 'No.', 'Maybe So.', 'Lol! You wish.', 'Ask again.', 'Hard to tell at the moment.', 'For sure.', 'No chance.', 'It is certain.', 'Do not feel like answering right now.']
+
+        #Title
+
+        self.title = Label(master, text = 'Magic 8 Ball', font= self.title_font, bg = 'red', fg = 'gold', relief = 'raised', borderwidth = 5)
+        self.title.grid(row=1, column = 1, columnspan = 2, sticky = 'w' + 'e', padx = 15, ipadx = 10, pady = 10)
+
+        #Instructions
+        self.instructions = Label(master, text = 'Please enter a yes or no question.', font = self.other_font)
+        self.instructions.grid(row = 2, column = 1, columnspan = 2, sticky = 'e' + 'w')
+
+        #Entry Widget
+        self.ask = Label(master, text = 'Ask your question here.', font = self.other_font)
+        self.ask.grid(row = 3, column = 1)
+
+        self.ask_entry = Entry(master, textvariable = self.entry_question)
+        self.ask_entry.grid(row = 3, column = 2)
+
+        #Answer
+        self.fortune = Label(master, textvariable = self.fortune_text, font = self.other_font)
+        self.fortune.grid( row = 4, column = 2)
+
+        #Button
+        self.button = Button(master, text = "Get Fortune.", command = lambda: self.fortune_text.set(self.fortunes[random.randrange(len(self.fortunes))]))
+        self.button.grid(row = 4, column = 1)
+
+
+
+
+if '__main__' == __name__:
+    root = Tk()
+    root.title('Magic 8 Ball')
+    my_app = App(root)
+    root.mainloop()
--- tkinter_universal_gravity_calculator.py
+++ tkinter_universal_gravity_calculator.py
@@ -19,3 +19,65 @@
 # - Add an exception for the possible entry of zero radius (ZeroDivisionError Exception)
 # - Make your app unique by changing 3 or more DIFFERENT style attributes or kwargs for your app.  Perhaps consider: fonts, color, padding, widths, etc).  Put a comment in your code to tell me what you changed. If you change the font for all the widgets, that is ONE thing.  If you add padx to all your app widgets, that is ONE thing.  If you change the color of all your blocks, that is ONE thing.
 
+
+#Padded, changed color, and chose a font
+
+from tkinter import *
+from tkinter.font import *
+
+class App():
+    def __init__(self, master):
+
+        self.title_font = Font(family='Curlz Mt', size=30)
+
+        #Declare variables
+        self.m1 = DoubleVar()
+        self.m2 = DoubleVar()
+        self.r = DoubleVar()
+        self.total = DoubleVar()
+
+        self.total.set(0)
+
+        # Title
+        self.title = Label(master, text='Universal Gravity Calculator', font = self.title_font, bg='red', fg='green', relief='raised', borderwidth=5)
+        self.title.grid(row=1, column=1, columnspan=2, sticky='w' + 'e', padx=20, ipadx=10, pady=10)
+
+        #m1 Label and Entry Widget
+        self.m1_label = Label(master, text = "Mass of Object One (kg)")
+        self.m1_label.grid(row=2, column = 1, sticky = 'e')
+
+        self.m1_entry  =Entry(master, textvariable = self.m1, width = 10, justify = CENTER)
+        self.m1_entry.grid(row = 2, column = 2, sticky = 'w')
+
+        #m2 Label and Entry Widget
+        self.m2_label = Label(master, text = "Mass of Object Two (kg)")
+        self.m2_label.grid(row=3, column = 1, sticky = 'e')
+
+        self.m2_entry =Entry(master, textvariable = self.m2, width = 10, justify = CENTER)
+        self.m2_entry.grid(row = 3, column = 2, sticky = 'w')
+
+        #radius Label and Entry Widget
+        self.r_label = Label(master, text = "Distance From Centers (m)")
+        self.r_label.grid(row=4, column = 1, sticky = 'e')
+
+        self.r_entry  =Entry(master, textvariable = self.r, width = 10, justify = CENTER)
+        self.r_entry.grid(row = 4, column = 2, sticky = 'w')
+
+        # Add total button and result label
+        self.total_button = Button(master, text = "Calculate Gravity", command = lambda: self.pressed_button())
+        self.total_button.grid(row = 5, column = 1, sticky = 'e')
+
+        self.total_label = Entry(master, textvariable = self.total, width = 10, justify = CENTER)
+        self.total_label.grid(row=5, column = 2, pady = 10, sticky = 'w')
+
+    def pressed_button(self):
+        try:
+            self.total.set((6.67 * 10 ** -11) * (self.m1.get() * self.m2.get() / self.r.get() ** 2))
+        except:
+            self.total.set("Error.")
+
+if '__main__' == __name__:
+    root = Tk()
+    root.title('Add two numbers')
+    my_app = App(root)
+    root.mainloop()
